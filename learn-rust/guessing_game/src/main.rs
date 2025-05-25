425672e9c996d033ff556f3063c209058c7849ef
Andrew Hennis <dawnandrew100@gmail.com>
2025-05-25 11:01:20 -0400

    Upgraded guessing game

--- learn-rust/guessing_game/src/main.rs
+++ learn-rust/guessing_game/src/main.rs
@@ -1,37 +1,83 @@
 use rand::Rng;
 use std::cmp::Ordering;
-use std::io;
+use std::{io, str::FromStr};
 
-fn main() {
-    println!("Guess the number from 1 to 100!");
+#[derive(Clone, Copy)]
+enum Choice {
+    Yes,
+    No,
+}
 
-    let secret_number = rand::thread_rng().gen_range(1..=100);
+impl FromStr for Choice {
+    type Err = &'static str;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s.trim().to_lowercase().as_str() {
+            "y" | "yes" | "1" => Ok(Choice::Yes),
+            "n" | "no" | "2" => Ok(Choice::No),
+            _ => Err("Please enter either Yes or No."),
+        }
+    }
+}
+
+fn main() -> std::io::Result<()> {
+    let mut response = String::new();
     let mut num_guesses: u32 = 0;
+    let min = 1;
+    let max = 100;
+
+    println!("\nA new secret number has been chosen!");
+    println!("Guess the number from {min} to {max}!\n");
 
     loop {
-        println!("Please input your guess.");
+        let secret_number = rand::rng().random_range(min..=max);
 
-        let mut guess = String::new();
+        loop {
+            println!("Please input your guess!");
+            response.clear();
+            io::stdin().read_line(&mut response)?;
 
-        io::stdin()
-            .read_line(&mut guess)
-            .expect("Failed to read line");
+            let guess: usize;
+            match response.trim().parse::<usize>() {
+                Ok(num) if num >= min && num <= max => guess = num,
+                Ok(_) => {
+                    eprintln!("Number entered not within range {min}-{max}!");
+                    continue;
+                }
+                Err(err) => {
+                    eprintln!("{}", err);
+                    continue;
+                }
+            };
+            println!("You guessed: {guess}");
+            num_guesses += 1;
 
-        let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(_) => continue,
-        };
-        println!("You guessed: {guess}");
-        num_guesses += 1;
-
-        match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too small!"),
-            Ordering::Greater => println!("Too big!"),
-            Ordering::Equal => {
-                println!("You win!");
-                println!("You guessed the secret number in {num_guesses} guesses!");
-                break;
+            match guess.cmp(&secret_number) {
+                Ordering::Less => println!("\nToo small!\n"),
+                Ordering::Greater => println!("\nToo big!\n"),
+                Ordering::Equal => {
+                    println!("\nYou win!");
+                    println!("You guessed the secret number in {num_guesses} guesses!\n");
+                    break;
+                }
             }
+        }
+        let decision = loop {
+            println!("\nWould you like to guess again?\n1. Yes\n2. No");
+            response.clear();
+            io::stdin().read_line(&mut response)?;
+            match response.trim().parse::<Choice>() {
+                Ok(choice) => break choice,
+                Err(err) => eprintln!("{}", err),
+            }
+        };
+
+        match decision {
+            Choice::Yes => {
+                num_guesses = 0;
+                continue;
+            }
+            Choice::No => break Ok(()),
         }
     }
 }
