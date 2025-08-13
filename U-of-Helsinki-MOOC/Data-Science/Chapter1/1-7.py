c60c8c6686f92b4bc6d4fb49aca2cb98b8c661fa
Andrew Hennis <dawnandrew100@gmail.com>
2025-08-12 21:35:33 -0400

    squish

--- U-of-Helsinki-MOOC/Data-Science/Chapter1/1-7.py
+++ U-of-Helsinki-MOOC/Data-Science/Chapter1/1-7.py
@@ -4,11 +4,9 @@ def main():
         shape = input("Choose a shape (triangle, rectangle, circle): ").lower()
         if shape == "":
             break
-
         if shape not in available_shapes:
             print("Unknown shape")
             continue
-
         area = 0
         match shape:
             case "triangle":
@@ -17,9 +15,7 @@ def main():
                 area = circle()
             case "rectangle":
                 area = rectangle()
-
         print(f"The area of your {shape} is {area:.6f}")
-
         if not _keep_playing():
             break
 
@@ -27,13 +23,10 @@ def main():
 def circle():
     while True:
         radius = input("Give radius of the circle: ")
-
         invalids = _validate_input(radius)
-
         if invalids:
             print(f"{radius} is not a valid number for the radius.")
             continue
-
         pi = 3.14159265359
         return pi * float(radius) ** 2
 
@@ -42,9 +35,7 @@ def triangle():
     while True:
         base = input("Give base of the triangle: ")
         height = input("Give height of the triangle: ")
-
         invalids = _validate_input(base, height)
-
         if invalids:
             print(f"You entered the following invalid numbers: {invalids}")
             continue
@@ -55,13 +46,10 @@ def rectangle():
     while True:
         width = input("Give width of the rectangle: ")
         height = input("Give height of the rectangle: ")
-
         invalids = _validate_input(width, height)
-
         if invalids:
             print(f"You entered the following invalid numbers: {invalids}")
             continue
-
         return float(width) * float(height)
 
 
@@ -88,7 +76,6 @@ def _is_float(num: str) -> bool:
 def _keep_playing() -> bool:
     while True:
         proceed = input("Would you like to continue? ").lower()
-
         if proceed == "no" or proceed == "n":
             return False
         if proceed == "yes" or proceed == "y":
