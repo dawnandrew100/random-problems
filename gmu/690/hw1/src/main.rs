39e6aa2245f2081ea0ecbf7b3c80ba5ac921006a
Andrew Hennis <dawnandrew100@gmail.com>
2025-09-05 21:29:54 -0400

    answered hw1 code section

--- gmu/690/hw1/src/main.rs
+++ gmu/690/hw1/src/main.rs
@@ -1,36 +1,56 @@
 use plotters::prelude::*;
 
-const G: f64 = 9.8;
+const PI: f64 = 3.14159265359;
 
 fn main() {
-    let (t, v_array) = numerical_solution(0.64, 23.0, 0.0, 10.0, 15.0, 1.0);
-    println!(
-        "final time is {t} and the list of droplet_area is {:?}",
-        v_array
-    );
-    let _ = plot_points(&v_array);
+    let (t, v_array, rf) = droplet_num_sol(0.1, 0.0, 10.0, 3.0, 0.25);
+    println!("The radius of the droplet at the final time of {t} minutes is {rf:.4}mm^2.");
+    println!("the droplet volume at each time point is {:?}", v_array);
+    if let Err(e) = plot_points(&v_array) {
+        eprintln!("Plot failed: {}", e);
+    };
 }
 
-fn numerical_solution(
-    c: f64,
-    m: f64,
-    ti: f64,
-    tf: f64,
-    vi: f64,
-    dt: f64,
-) -> (f64, Vec<(f64, f64)>) {
+fn droplet_num_sol(k: f64, ti: f64, tf: f64, ri: f64, dt: f64) -> (f64, Vec<(f64, f64)>, f64) {
+    // k = evaporation rate, ri = initial radius
+    // ti = initial time, tf = final time, dt = step size
+    // a = surface area
+    // Equation = dv/dt = -kA
+    // volume of hemisphere = v = 2/3 * pi * r^3
+    // volume in terms of r = cuberoot((v*3)/(2*pi))
+    // Surface area of hemisphere= a = 2 * pi * r^2
     let mut t = ti;
-    let mut v = vi;
+    let mut r = ri;
+    let mut v = (2.0 / 3.0) * PI * r * r * r;
+    let mut a = 2.0 * PI * r * r;
     let n = (tf - ti) / dt;
 
     let mut v_array = Vec::new();
+    // Add initial conditions to vector
+    v_array.push((t, v));
+    // Calculate remaining volumes
     for _ in 1..=n as usize {
-        let dvdt = G - c * v / m;
+        let dvdt = -k * a;
         v = v + dvdt * dt;
         t = t + dt;
+        r = ((3.0 * v) / (2.0 * PI)).cbrt();
+        a = 2.0 * PI * r * r;
+        if v <= 0.0 {
+            v_array.push((t, 0.0));
+            break;
+        };
         v_array.push((t, v));
     }
-    (t, v_array)
+    let time_rem = ((tf - t) / dt) as usize;
+    // Prevents unnecessary calculations when droplet is gone
+    if time_rem > 0 {
+        for _ in 1..=time_rem {
+            t += dt;
+            v_array.push((t, 0.0));
+        }
+    }
+
+    (t, v_array, r)
 }
 
 fn plot_points(droplet_area: &Vec<(f64, f64)>) -> Result<(), Box<dyn std::error::Error>> {
@@ -51,8 +71,8 @@ fn plot_points(droplet_area: &Vec<(f64, f64)>) -> Result<(), Box<dyn std::error:
         .map(|p| p.1)
         .fold(f64::NEG_INFINITY, f64::max);
 
-    let root =
-        BitMapBackend::new("plotters-doc-data/droplet_area.png", (1280, 960)).into_drawing_area();
+    let root = BitMapBackend::new("plotters-doc-data/droplet_area_10_min.png", (1280, 960))
+        .into_drawing_area();
     let _ = root.fill(&WHITE);
     let root = root.margin(10, 10, 40, 110);
     // After this point, we should be able to construct a chart context
@@ -83,8 +103,7 @@ fn plot_points(droplet_area: &Vec<(f64, f64)>) -> Result<(), Box<dyn std::error:
         &RED,
         &|c, s, st| {
             return EmptyElement::at(c)    // We want to construct a composed element on-the-fly
-            + Circle::new((0,0),s,st.filled()) // At this point, the new pixel coordinate is established
-            + Text::new(format!("{:.2}, {:.2}", c.0, c.1), (15, -10), ("sans-serif", 20).into_font());
+            + Circle::new((0,0),s,st.filled());
         },
     ))?;
     root.present()?;
