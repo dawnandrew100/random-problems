3f2f860b6abc5112299f24d092966e45f13498ef
Andrew Hennis <81731313+dawnandrew100@users.noreply.github.com>
2025-10-20 08:58:32 -0400

    Refactor interpolation function and update main logic
    Refactored the new_int function to return coefficients, evaluated point, and error vector. Updated main function to call new_int with a specific point and degree, and print results.

--- gmu/690/hw6/interpolation/src/main.rs
+++ gmu/690/hw6/interpolation/src/main.rs
@@ -2,15 +2,16 @@ fn main() {
     let x: Vec<f64> = vec![1.6, 2.0, 2.5, 3.2, 4.0, 4.5];
     let fx: Vec<f64> = vec![2.0, 8.0, 14.0, 15.0, 8.0, 2.0];
 
-    new_int(&x, &fx, 6, 10.0);
+    let point = 2.8;
+    let degree = 5;
+    let (coeffs, evaled_point, ea) = new_int(&x, &fx, degree, point);
+    println!("Interpolated coeffs for degree {degree} polynomial = {coeffs:?}");
+    println!("Estimated error vector = {ea:?}");
+    println!("Function evaluated at point {point} = {evaled_point:.5}");
 }
 
-/*
- * THIS IMPLEMENTATION IS NOT COMPLETE
- * NEEDS TO BE REWORKED
- */
-
-fn new_int(x: &Vec<f64>, y: &Vec<f64>, n: usize, xi: f64) {
+fn new_int(x: &Vec<f64>, y: &Vec<f64>, n: usize, eval_point: f64) -> (Vec<f64>, f64, Vec<f64>) {
+    let n = n + 1;
     let mut fdd = vec![vec![0.0; n]; n];
     let mut yint = vec![0.0; n];
     let mut ea = vec![0.0; n];
@@ -25,10 +26,25 @@ fn new_int(x: &Vec<f64>, y: &Vec<f64>, n: usize, xi: f64) {
     let mut xterm = 1.0;
     yint[0] = fdd[0][0];
     for i in 1..n {
-        xterm *= xi - x[i];
+        xterm *= eval_point - x[i];
         let yint2 = yint[i - 1] + fdd[0][i] * xterm;
         ea[i - 1] = yint2 - yint[i - 1];
         yint[i] = yint2;
     }
-    println!("{fdd:?}\n{yint:?}\n{ea:?}");
+
+    let mut expanded_poly: Vec<f64> = vec![fdd[0][0]];
+    let mut degree = 1;
+    let mut term = 0.0;
+    for i in 1..n {
+        term += fdd[0][i];
+        for j in 0..degree {
+            term *= eval_point - x[j];
+        }
+        expanded_poly.push(term);
+        degree += 1;
+        term = 0.0;
+    }
+    let evaluated_point = expanded_poly.into_iter().sum::<f64>();
+
+    (fdd[0].clone(), evaluated_point, ea)
 }
