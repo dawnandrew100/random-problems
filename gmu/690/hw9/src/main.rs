fdef91842240101c25e4024a40e20c93a4d1b95d
Andrew Hennis <dawnandrew100@gmail.com>
2025-11-16 17:37:37 -0500

    hw9

--- gmu/690/hw9/src/main.rs
+++ gmu/690/hw9/src/main.rs
@@ -1,32 +1,67 @@
-use spindalis::utils::arr2D::{Arr2D, Rounding};
+use spindalis::utils::{
+    Arr2DError,
+    arr2D::{Arr2D, Rounding},
+};
 
 fn main() {
-    let matrix = Arr2D::from(&[
+    // Matrix with test data from
+    // Example 27.7 and 27.8 from the book
+    let matrix_1 = Arr2D::from(&[
         [3.556, -1.778, 0.0],
         [-1.778, 3.556, -1.778],
         [0.0, -1.778, 3.556],
     ]);
-    let (value, vector) = eigenvalue_power_method(&matrix, 1e-10);
-    println!(
-        "Value = {value:.4}\nVector:\n{}\n",
-        vector.round_to_decimal(5)
-    );
+    let matrix_2 = Arr2D::from(&[[2.0, 8.0, 10.0], [8.0, 4.0, 5.0], [10.0, 5.0, 7.0]]);
 
-    let inverse_matrix = matrix.inverse().unwrap();
-    let (value, vector) = eigenvalue_power_method(&inverse_matrix, 1e-10);
-    println!(
-        "Value = {value:.4}\nVector:\n{}",
-        vector.round_to_decimal(5)
-    );
+    let matrices = vec![
+        (matrix_1, "Examples 27.7 and 27.8"),
+        (matrix_2, "Homework questions 27.11 and 27.12"),
+    ];
+
+    for (matrix, title) in matrices {
+        println!("\n{title}\n");
+        let result = eigenvalue_power_method(&matrix, 1e-10);
+        match result {
+            Ok((value, vector)) => println!(
+                "Largest Eigen Value = {value:.4}\nVector:\n{}\n",
+                vector.round_to_decimal(5)
+            ),
+            Err(e) => eprintln!("{e:?}"),
+        }
+
+        let inverse_res = matrix.inverse();
+        match inverse_res {
+            Ok(inverse_matrix) => {
+                let result = eigenvalue_power_method(&inverse_matrix, 1e-10);
+                match result {
+                    Ok((value, vector)) => println!(
+                        "Smallest Eigen Value = {:.4}\nVector:\n{}",
+                        (1_f64 / value).sqrt(),
+                        vector.round_to_decimal(5),
+                    ),
+                    Err(e) => eprintln!("{e:?}"),
+                }
+            }
+            Err(e) => {
+                eprintln!(
+                    "Error calculating the inverse for the following matrix:\n{matrix}\n{e:?}"
+                )
+            }
+        }
+    }
 }
 
-fn eigenvalue_power_method(matrix: &Arr2D<f64>, es: f64) -> (f64, Arr2D<f64>) {
+fn eigenvalue_power_method<M>(matrix: M, es: f64) -> Result<(f64, Arr2D<f64>), Arr2DError>
+where
+    M: TryInto<Arr2D<f64>, Error = Arr2DError>,
+{
+    let matrix: Arr2D<f64> = matrix.try_into()?;
     let initial_eigen_vector = Arr2D::from(&[[1.0], [1.0], [1.0]]);
-    let mut eigen_vector = matrix * &initial_eigen_vector;
+    let mut eigen_vector = &matrix * &initial_eigen_vector;
     let mut eigen_value = eigen_vector.max().unwrap();
     eigen_vector = eigen_vector / eigen_value;
     loop {
-        eigen_vector = matrix * eigen_vector;
+        eigen_vector = &matrix * eigen_vector;
         let next_eigen_value = eigen_vector.max().unwrap();
 
         let ea = ((next_eigen_value - eigen_value) / next_eigen_value).abs();
@@ -37,5 +72,5 @@ fn eigenvalue_power_method(matrix: &Arr2D<f64>, es: f64) -> (f64, Arr2D<f64>) {
             break;
         }
     }
-    (eigen_value, eigen_vector)
+    Ok((eigen_value, eigen_vector))
 }
